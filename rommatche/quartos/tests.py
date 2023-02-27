182d30846e79023edeea380e2bace1ea16b88659
Beatrizresteves <Beatrizresteves@github.com>
2023-02-27 17:36:48 -0300

    Retira teste

--- rommatche/quartos/tests.py
+++ rommatche/quartos/tests.py
@@ -3,5 +3,5 @@ from django.test import TestCase
 # Create your tests here.
 
 
-def bate_endpoint(db, client):
-    client.post(api/quartos/list_quartos)
+# def bate_endpoint(db, client):
+#     client.post(api/quartos/list_quartos)
